87240337e023997274bc03afdd8f469854270397
MrVanDaime - Home <joaocamini@live.com>
2025-01-20 21:28:48 -0300

    add miau command

---
+++ src/commands/utility/miau.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const { SlashCommandBuilder } = require('discord.js');
+
+const validUsers = process.env.USERS.split(',');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('miau')
+		.setDescription('Fala do gato')
+		.addStringOption((option) =>
+			option
+				.setName('message')
+				.setDescription('Fala do gato')
+				.setRequired(true)
+				.setMinLength(1)
+				.setMaxLength(255),
+		),
+	async execute(interaction) {
+		// Define pessoas para uso do comando
+		if (!validUsers.includes(interaction.user.id)) {
+			return await interaction.reply({
+				content:
+					'Erro: Você precisa doar 66g de cinzas de gato para usar esse comando.',
+				ephemeral: false,
+			});
+		}
+
+		const message = interaction.options.getString('message');
+
+		console.log(interaction.channel);
+
+		await interaction.channel.send(message);
+
+		await interaction.reply({
+			content: message,
+			ephemeral: true,
+		});
+	},
+};
