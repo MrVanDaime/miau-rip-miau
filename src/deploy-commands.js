94425252d89befa2335de70733ceeede54aa4738
MrVanDaime - Home <joaocamini@live.com>
2025-02-01 11:44:47 -0300

    sort code with functions

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -3,40 +3,41 @@ require('dotenv').config();
 const { REST, Routes } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
-
 const logger = require('./utils/logger');
 
-const commands = [];
+const loadCommands = async () => {
+	const commands = [];
 
-// Grab all the command folders from the commands directory you created earlier
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+	// Grab all the command folders from the commands directory you created earlier
+	const foldersPath = path.join(__dirname, 'commands');
+	const commandFolders = fs.readdirSync(foldersPath);
 
-for (const folder of commandFolders) {
-	// Grab all the command files from the commands directory you created earlier
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs
-		.readdirSync(commandsPath)
-		.filter((file) => file.endsWith('.js'));
-	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			commands.push(command.data.toJSON());
-		} else {
-			logger.info(
-				`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
-			);
+	for (const folder of commandFolders) {
+		// Grab all the command files from the commands directory you created earlier
+		const commandsPath = path.join(foldersPath, folder);
+		const commandFiles = fs
+			.readdirSync(commandsPath)
+			.filter((file) => file.endsWith('.js'));
+		// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+		for (const file of commandFiles) {
+			const filePath = path.join(commandsPath, file);
+			const command = require(filePath);
+			if ('data' in command && 'execute' in command) {
+				commands.push(command.data.toJSON());
+			} else {
+				logger.info(
+					`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
+				);
+			}
 		}
 	}
-}
 
-// Construct and prepare an instance of the REST module
-const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+	return commands;
+};
 
-// and deploy your commands!
-(async () => {
+const deployCommands = async (commands) => {
+	const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+
 	try {
 		logger.info(
 			`Started refreshing ${commands.length} application (/) commands.`,
@@ -52,7 +53,12 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 			`Successfully reloaded ${data.length} application (/) commands.`,
 		);
 	} catch (error) {
-		// And of course, make sure you catch and log any errors!
 		logger.error(error);
 	}
+};
+
+// deploy commands
+(async () => {
+	const commands = await loadCommands();
+	await deployCommands(commands);
 })();
