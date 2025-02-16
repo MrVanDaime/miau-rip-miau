118bd5a689b72a610b711b5ece43f80a55e58cde
MrVanDaime - Home <joaocamini@live.com>
2025-02-15 21:27:34 -0300

    feat(bonk): add proper select for users

--- src/commands/utility/bonk.js
+++ src/commands/utility/bonk.js
@@ -7,13 +7,11 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('bonk')
 		.setDescription('Bonk do gato')
-		.addStringOption((option) =>
+		.addUserOption((option) =>
 			option
 				.setName('user')
 				.setDescription('Quem vai levar bonk?')
-				.setRequired(true)
-				.setMinLength(17)
-				.setMaxLength(20),
+				.setRequired(true),
 		)
 		.addStringOption((option) =>
 			option
@@ -24,19 +22,12 @@ module.exports = {
 				.setMaxLength(50),
 		),
 	async execute(interaction) {
-		const userId = interaction.options.getString('user');
+		const userId = interaction.options.getUser('user').id;
 		const reason = interaction.options.getString('reason');
 
 		if (validUsers.includes(userId)) {
 			return await interaction.reply({
 				content: 'Não maltrate meus irmãos também 😿',
-			});
-		}
-
-		// Valid snowflake id
-		if (!/^\d{17,20}$/.test(userId)) {
-			return await interaction.reply({
-				content: 'Esse aí já está com minha mamãe 😇',
 			});
 		}
 
